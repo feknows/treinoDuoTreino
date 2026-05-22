9ca398297ab7d6325ebacdc2895d8abd7e51da23
feknows <fapalmeida18@gmail.com>
2026-05-22 10:55:29 -0300

    Add supabase anon key

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://aqgjxbgkxetpvjqtweii.supabase.co'
-const supabaseAnonKey = 'sb_secret_AMwcMrAb8pD9NydB9nFBkA_G8s4xWTY'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxZ2p4YmdreGV0cHZqcXR3ZWlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NTQ4MzQsImV4cCI6MjA5NTAzMDgzNH0.tLvHMc7Iz7TgMY5U25PMZWUXH4W2Ebq2sn9gcvviPAw'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
